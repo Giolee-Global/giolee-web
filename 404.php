86fd3a35d0256cca1eee4d786150f43e4ce09d90
codystack <ebuzzadvert@gmail.com>
2025-02-18 11:08:06 +0100

    404 error page  added

---
+++ 404.php
@@ -0,0 +1,36 @@
+<?php
+include "./components/header.php";
+?>
+
+    <div class="error section-padding-three">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="error-page">
+                        <h1>4<span>0</span>4</h1>
+                        <h2>Oops! Page not found.</h2>
+                        <p>The page you are looking for is not available or doesn’t belong to this website!</p>						
+                        <a class="build_button" href="./">Back to Home <i class="flaticon-right-up"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+	<script src="assets/js/jquery-3.6.0.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/jquery.counterup.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/progressbar.min.js"></script>
+	<script src="assets/js/jquery.magnific-popup.min.js"></script>
+	<script src="assets/js/swiper-bundle.min.js"></script>
+	<script src="assets/js/jquery.waypoints.min.js"></script>
+	<script src="assets/js/isotope.pkgd.min.js"></script>
+	<script src="assets/js/wow.min.js"></script>
+	<script src="assets/js/gsap.js"></script>
+	<script src="assets/js/scroll-trigger.js"></script>
+	<script src="assets/js/split-text.js"></script>
+	<script src="assets/js/custom.js"></script>
+</body>
+
+</html>
