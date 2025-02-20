1da676e1d9912c6127a3b2e31954249c907aee3c
codystack <ebuzzadvert@gmail.com>
2025-02-20 16:29:29 +0100

    certifications page added

---
+++ certifications.php
@@ -0,0 +1,59 @@
+<?php
+include "./components/header.php";
+include "./components/navbar.php";
+?>
+
+    <div class="breadcrumb__area" style="background-image: url('assets/img/certifications.jpg');">
+		<div class="container">
+			<div class="row">
+				<div class="col-xl-12">
+					<div class="breadcrumb__area-content">
+						<h2>Certifications</h2>
+						<ul>
+							<li><a href="./">Home</a><i class="fa-regular fa-angle-right"></i></li>
+							<li>Certifications</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+    <div class="two__columns section-padding-three">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 mt-25 mb-50 wow fadeInUp" data-wow-delay=".4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
+                    <div class="portfolio__two-item">
+                        <img src="assets/img/certificates/cert1.jpeg" alt="image">
+                    </div>
+                </div>
+                <div class="col-md-4 mt-25 mb-50 wow fadeInUp" data-wow-delay=".4s" style="visibility: visible; animation-delay: 0.7s; animation-name: fadeInUp;">
+                    <div class="portfolio__two-item">
+                        <img src="assets/img/certificates/cert4.jpeg" alt="image">
+                    </div>
+                </div>
+                <div class="col-md-4 mt-25 mb-50 wow fadeInUp" data-wow-delay=".4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
+                    <div class="portfolio__two-item">
+                        <img src="assets/img/certificates/cert3.jpeg" alt="image">
+                    </div>
+                </div>
+                <div class="col-md-4 mt-25 mb-50 wow fadeInUp" data-wow-delay=".7s" style="visibility: visible; animation-delay: 0.7s; animation-name: fadeInUp;">
+                    <div class="portfolio__two-item">
+                        <img src="assets/img/certificates/cert3.jpeg" alt="image">
+                    </div>
+                </div>
+                <div class="col-md-4 mt-25 mb-50 wow fadeInUp" data-wow-delay=".7s" style="visibility: visible; animation-delay: 0.7s; animation-name: fadeInUp;">
+                    <div class="portfolio__two-item">
+                        <img src="assets/img/certificates/cert5.jpg" alt="image">
+                    </div>
+                </div>
+                <!-- <div class="col-md-4 mt-25 mb-50 wow fadeInUp" data-wow-delay=".7s" style="visibility: visible; animation-delay: 0.7s; animation-name: fadeInUp;">
+                    <div class="portfolio__two-item">
+                        <img src="assets/img/certificates/cert3.jpeg" alt="image">
+                    </div>
+                </div> -->
+            </div>
+        </div>
+    </div>
+
+<?php include "./components/footer.php"; ?>
