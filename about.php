fa1552c7dac729ffd03101a441e997c5cfc5c9b6
codystack <ebuzzadvert@gmail.com>
2025-05-12 10:11:15 +0100

    modification made

--- about.php
+++ about.php
@@ -25,11 +25,155 @@ include "./components/navbar.php";
             <div class="row">
                 <div class="col-lg-6 lg-mb-25">
                     <div class="mission__area-left mr-40 xl-mr-0">
+                        <h2 class="title_split_anim">Protecting Nature, Sustaining the Environment</h2>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="mission__area-ri```php
+<?php
+include "./components/header.php";
+include "./components/navbar.php";
+?>
+
+
+    <div class="breadcrumb__area" style="background-image: url('assets/img/about.jpg');">
+		<div class="container">
+			<div class="row">
+				<div class="col-xl-12">
+					<div class="breadcrumb__area-content">
+						<h2>About us</h2>
+						<ul>
+							<li><a href="./">Home</a><i class="fa-regular fa-angle-right"></i></li>
+							<li>About us</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+    <div class="mission__area section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 lg-mb-25">
+                    <div class="mission__area-left mr-40 xl-mr-0">
                         <h2 class="title_split_anim">Protecting Nature, Sustaining the Environment.</h2>
                     </div>
                 </div>
                 <div class="col-lg-6">
                     <div class="mission__area-right">
+                        <div class="row">
+                            <div class="col-md-12 md-mb-25 wow fadeInUp" data-wow-delay=".6s">
+                                <div class="experience__area-list-item">
+                                    <div>
+                                        <p class="wow fadeInUp" data-wow-delay=".6s">“Giolee Global Resources Limited is a fully indigenous company, duly incorporated in Nigeria on November 5, 2004. The company is committed to providing solutions that protect nature and sustain the environment.”</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="about__five section-padding pt-0">
+        <div class="container">
+            <div class="row al-center">
+                <div class="col-lg-5 lg-mb-25">
+                    <div class="about__five-image wow img_left_animation">
+                        <img src="assets/img/about_img.jpg" alt="image">
+                    </div>
+                </div>
+                <div class="col-lg-7">
+                    <div class="about__five-right ml-70 xl-ml-0">
+                        <div class="about__five-right-title mb-10">
+                            <span class="subtitle wow fadeInLeft" data-wow-delay=".4s">About Giolee</span>
+                            <h2 class="title_split_anim">Protecting the Earth, Sustaining Nature.</h2>
+                        </div>
+                        <p class="wow fadeInUp" data-wow-delay=".2s">We are fully aware of the diverse environmental challenges in a vast territory like the Nigerian environment, so we operate in total compliance with local, International, statutory and regulatory/legislative standards to bring positive impact to the environment.</p>
+                        <p class="wow fadeInUp" data-wow-delay=".4s">As a promoter of local content, we strongly uphold the practice of transfer of skills/knowledge and capacity with certifications to indigenous labor force as we deliver a world-class quality and timely result in all our projects.</p>
+                        <p class="wow fadeInUp" data-wow-delay=".6s">To achieve all these, we deploy state-of-the-art expertise and equipment on all projects, partnering with over 18 internationally viable and competent companies with global record in all our operations.</p>
+                        <p class="wow fadeInUp" data-wow-delay=".8s">In all our sensitive and major projects, we align with our international partners and collaborate with them to execute such projects with their full technical support and expertise.</p>
+                        <p class="wow fadeInUp" data-wow-delay=".10s">In all this, our core value is to <b>“Act Globally and Play Locally with an intent of putting customer first”</b></p>
+                        <!-- <div class="item_bounce">
+                            <a class="build_button mt-20" href="projects">See our projects<i class="flaticon-right-up"></i></a>
+                        </div> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+	<div class="video__two" style="background-image: url('assets/img/about.jpg');">
+		<div class="container">
+			<div class="row">
+				<div class="col-xl-8 mx-auto">
+					<div class="item_bounce">
+                        <h3 class="text-white mb-3">QUALITY POLICY</h3>
+                        <p class="text-white lead">Giolee Global Resources Limited is a Legal Entity incorporated under the Corporate Affairs Commission (CAC) with registration No. RC 610624. We are committed to providing high-quality services in Oil Spill Clean-up, Remediation, Environmental Assessment and Consultancy, Training Services, and Laboratory Services that meet and/or surpass all quality expectations of customers, statutory, regulatory, and other relevant interested parties.</p>
+                        <p class="text-white lead">We endeavour to develop the necessary skills and build an attitude essential for a risk-based thinking approach to work towards achieving our objectives.</p>
+                        <p class="text-white lead">GIOLEE adheres strictly to the standards of ISO9001:2015 and ISO/IEC 17025 and maintains the highest level of integrity, swift responsiveness, and shall ensure this by continuously implementing, maintaining, and improving its Quality Management System in order to reach and possibly exceed clients' satisfaction. GIOLEE shall also build capacity in its area of specialization and maintain international best practice in all her services.</p>
+                        <p class="text-white lead">We shall ensure that this policy is communicated, understood, implemented, documented, and made available at all levels within the organization and to all relevant interested parties.</p>			
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+    
+    <div class="certification section-padding pt-0">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-4 col-lg-5 lg-mb-25">
+                    <div class="certification-left pt-60 pb-0">
+                        <h3 class="title_split_anim">Our Vision</h3>
+                        <p class="wow fadeInUp" data-wow-delay=".2s">To be the biggest environmental company in Nigeria/Africa and one of the best in the world.</p>
+                        <h3 class="title_split_anim">Our Mission</h3>
+                        <p class="wow fadeInUp" data-wow-delay=".2s">Our mission is to play globally and act locally to create a world-class environmental solution by offering excellent values for money in order to achieve and maintain a reputation in supplying of environmental and allied services to our clients.</p>
+                    </div>
+                </div>
+                <div class="col-xl-8 col-lg-7">
+                    <div class="certification-right">
+                        <img class="wow img_top_animation" src="assets/img/who-we-are.jpg" alt="image">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row mt-80">
+                        <div class="col-md-4 md-mb-25 wow fadeInUp" data-wow-delay=".4s">
+                            <div class="industry__four-left-item borders pr-10 lg-pr-0">
+                                <h5>Capacity</h5>
+                                <p>Giolee has the capacity to support a diverse client base—from industry-based projects to national and international government projects. Our experience gives us a competitive edge in supporting these clients, enabling us to meet their immediate needs while driving long-term growth and development.</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4 md-mb-25 wow fadeInUp" data-wow-delay=".4s">
+                            <div class="industry__four-left-item borders pr-10 lg-pr-0">
+                                <h5>Projects</h5>
+                                <p>We deploy state-of-the-art expertise and equipment on all projects, partnering with over 18 internationally viable and competent companies with global record in all their operations. We are consistent in delivering world-class quality results across all our projects.</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4 wow fadeInUp" data-wow-delay=".8s">
+                            <div class="industry__four-left-item pl-20 xl-pl-0">
+                                <h5>Specialists</h5>
+                                <p>We are equipped with a team of skilled professionals, advanced equipment, and robust resources, ready to execute any project within our service areas to meet our clients' needs effectively. We are ISO 9001:2015 certified and as such all our operations are in line with best Industrial and International standards.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section-padding">
+		<div class="container">
+			<div class="row al-center">
+				<div class="col-xl-6 col-lg-5 lg-mb-25">
+					<div class="choose__three-left t-right mr-60 xl-mr-0">
+						<img class="lg_img_full wow img_left_animation" src="assets/img/how_we_work.jpg" alt="image">
+					</div>
+				</div>
+				<div class="col-xl-6 col-lgght">
                         <div class="row">
                             <div class="col-md-12 md-mb-25 wow fadeInUp" data-wow-delay=".6s">
                                 <div class="experience__area-list-item">
