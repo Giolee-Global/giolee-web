4ea8443e091e5477cebed27e4a6a84aaaa6f0116
codystack <ebuzzadvert@gmail.com>
2025-06-20 12:34:12 +0100

    Page modified

--- oil-spill-clean-up-and-remediation.php
+++ oil-spill-clean-up-and-remediation.php
@@ -1,18 +1,19 @@
 <?php
 include "./components/header.php";
 include "./components/navbar.php";
+include_once "./auth/data1.php";
 ?>
 
-    <div class="breadcrumb__area" style="background-image: url('assets/img/spill.jpg');background-position: center;">
+    <div class="breadcrumb__area" style="background-image: url('https://backoffice.giolee.com/<?php echo $breadcrumb_filePath; ?>');background-position: center;">
 		<div class="container">
 			<div class="row">
 				<div class="col-xl-12">
 					<div class="breadcrumb__area-content">
-						<h2>Oil Spill Clean Up</br>& Remediation</h2>
+						<h2><?php echo $title; ?></h2>
 						<ul>
 							<li><a href="./">Home</a><i class="fa-regular fa-angle-right"></i></li>
                             <li class="text-white">Services<i class="fa-regular fa-angle-right"></i></li>
-							<li>Oil Spill Clean Up & Remediation</li>
+							<li><?php echo $title; ?></li>
 						</ul>
 					</div>
 				</div>
@@ -26,82 +27,38 @@ include "./components/navbar.php";
                 <?php include "./components/services-menu.php"; ?>
                 <div class="col-lg-8">
                     <div class="services__details-area">
-                        <img src="assets/img/service/spill.jpg" alt="image">
-                        <h3 class="mt-25 mb-20">Oil Spill Clean Up & Remediation</h3>
-                        <p class="mb-20">Giolee has industry expertise and experience in the remediation of hydrocarbon contaminated sites. We have rendered various clean-up/remediation projects services to reputable clients in the oil and gas industry in Nigeria. Some of the project sites include swamp and land locations with hydrocarbon contamination at varying depths beyond one meter. Our projects also involve shoreline clean-up and systematic mangrove vegetation cleaning.</p>
-                        <p class="mb-20">Giolee uses a range of Bio-remediation Additive combination Types  for her projects and they include:</p>
-                        <p>
-                            <b>(a) OSE II (Oil Spill Eater II):</b>
-                            OSE II is a biological enzyme product. OSE II primarily contains Bio-surfactants and nutrients that enhance the activity of indigenous (naturally occurring) bacteria to break complex hydrocarbon chains and convert it to CO2 and water.</p>
-                        <p>
-                            <b>(b) Petrol Cleanze:</b>
-                            Petrol Cleanze is another bioremediation additive designed to address a wide range of petroleum hydrocarbons in soil and groundwater.
-                            It functions similarly by providing nutrients and other amendments that promote the growth and activity of hydrocarbon-degrading bacteria, thereby aiding and quickening the remediation process (this is made by Regenesis USA/UK).
-                        </p>
-                        <p>
-                            <b>(c) Ergofito Aqua:</b>
-                            Ergofito Aqua is another highly effective in-situ remediation additive for contaminated soils. It can break down complex petroleum hydrocarbon chain between C1 to C35.
-                            Ergofito Aqua attacks the hydrocarbons directly and facilitates their conversion into humus. Humus is a stable, decomposed organic matter that is beneficial for soil health and enhances soil nutrients for healthy plant growth (this is made by Ergofito in South Africa)
-                        </p>
-                        <p class="mb-20">Giolee has partnership with Oil Spill Eater II Corporation (OSE II Corporation) a unique first response bioremediation agent, safe and effective on all types of oil and environments and is completely non-toxic. OSE II breaks down the molecular structure of hydrocarbons through the effects of bio surfactants and enzymes.
-                        Remediation of a hydrocarbon Impacted Site can be achieved through many methods. However the methods Giolee employs for Clean-up of land /soil remediation and swamp/creek are:</p>
-                        <h4 class="mb-20">Methodology for Land/Soil Remediation</h4>
-                        <ul>
-                            <li>Site Preparation.</li>
-                            <li>Tilling of impacted soil.</li>
-                            <li>Excavation of Hot Spots.</li>
-                            <li>Bulking of impacted soil.</li>
-                            <li>Land Farming of bulked soil.</li>
-                            <li>Construction/breakdown of windrows and soil. homogenization with the application of Oil Spill Eater II to remove the contaminate from the soil.</li>
-                        </ul>
-                        <div class="row mt-40 mb-40">
-                            <div class="col-sm-6 sm-mb-25">
-                                <img class="img_full" src="assets/img/service/spill1.jpg"alt="image">
-                            </div>
-                            <div class="col-sm-6">
-                                <img class="img_full" src="assets/img/service/spill3.jpg" alt="image">
-                            </div>
+                        <img src="https://backoffice.giolee.com/<?php echo $hero_filePath; ?>" alt="image">
+                        <h3 class="mt-25 mb-20"><?php echo $title; ?></h3>
+                        <div>
+                            <?php echo $firstParagraph; ?>
                         </div>
-                        <h4 class="mb-20">Methodology for creek Remediation</h4>
-                        <ul>
-                            <li>Site assessment/Preparation.</li>
-                            <li>Clean –Up/Clearing of impacted vegetation.</li>
-                            <li>Mopping of stained vegetation with absorbent pad soaked in OSE II.</li>
-                            <li>Application OSE II on the imparted area.</li>
-                            <li>Application of OSE II on contaminated water, lake and sea.</li>
-                            <li>Raking/Packing of oily Debris.</li>
-                            <li>Moping and trimming Of Stained Vegetation with absorbent pads.</li>
-                        </ul>
-                        <p class="mb-20">Giolee has industry expertise and experience in the remediation of hydrocarbon contaminated sites. We have rendered various clean-up/remediation projects services to reputable clients in the oil and gas industry in Nigeria. Some of the project sites include swamp and land locations with hydrocarbon contamination at varying depths beyond one meter. Our projects also involve shoreline clean-up and systematic mangrove vegetation cleaning.</p>
-                        <p class="mb-20">Giolee has partnership with Oil Spill Eater II Corporation (OSE II Corporation) a unique first response bioremediation agent, safe and effective on all types of oil and environments and is completely non-toxic. OSE II breaks down the molecular structure of hydrocarbons through the effects of bio surfactants and enzymes.
-                        Remediation of a hydrocarbon Impacted Site can be achieved through many methods. However the methods Giolee employs for Clean-up of land /soil remediation and swamp/creek are:</p>
-                        <h4 class="mb-20">Methodology for Land/Soil Remediation</h4>
-                        <ul>
-                            <li>Site Preparation.</li>
-                            <li>Tilling of impacted soil.</li>
-                            <li>Excavation of Hot Spots.</li>
-                            <li>Bulking of impacted soil.</li>
-                            <li>Land Farming of bulked soil.</li>
-                            <li>Construction/breakdown of windrows and soil. homogenization with the application of Oil Spill Eater II to remove the contaminate from the soil.</li>
-                        </ul>
                         <div class="row mt-40 mb-40">
-                            <div class="col-sm-6 sm-mb-25">
-                                <img class="img_full" src="assets/img/service/spill4.jpg"alt="image">
-                            </div>
-                            <div class="col-sm-6">
-                                <img class="img_full" src="assets/img/service/spill5.jpg" alt="image">
-                            </div>
+                            <?php foreach ($media_images as $imagePath): ?>
+                                <div class="col-sm-6 mb-20">
+                                    <img class="img_full" src="https://backoffice.giolee.com/<?php echo $imagePath; ?>"alt="image">
+                                </div>
+                            <?php endforeach; ?>
                         </div>
-                        <h4>Methodology for creek Remediation</h4>
-                        <ul>
-                            <li>Site assessment/Preparation.</li>
-                            <li>Clean –Up/Clearing of impacted vegetation.</li>
-                            <li>Mopping of stained vegetation with absorbent pad soaked in OSE II.</li>
-                            <li>Application OSE II on the imparted area.</li>
-                            <li>Application of OSE II on contaminated water, lake and sea.</li>
-                            <li>Raking/Packing of oily Debris.</li>
-                            <li>Moping and trimming Of Stained Vegetation with absorbent pads.</li>
-                        </ul>
+                        <div>
+                            <?php echo $secondParagraph; ?>
+                        </div>
+                        <div class="row mt-40 mb-40">
+                            <?php foreach ($media_two_images as $image_two_Path): ?>
+                                <div class="col-sm-6 mb-20">
+                                    <img class="img_full" src="https://backoffice.giolee.com/<?php echo $image_two_Path; ?>"alt="image">
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
+                        <div>
+                            <?php echo $thirdParagraph; ?>
+                        </div>
+                        <div class="row mt-40 mb-40">
+                            <?php foreach ($media_three_images as $image_three_Path): ?>
+                                <div class="col-sm-6 mb-20">
+                                    <img class="img_full" src="https://backoffice.giolee.com/<?php echo $image_three_Path; ?>"alt="image">
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
                     </div>
                 </div>
             </div>
