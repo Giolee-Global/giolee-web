0104e47921fce8ad6940bddc71f0a05bd178b61c
codystack <ebuzzadvert@gmail.com>
2025-06-21 12:06:38 +0100

    certificate modified

--- certifications.php
+++ certifications.php
@@ -2,18 +2,6 @@
 require_once "./config/db.php";
 include "./components/header.php";
 include "./components/navbar.php";
-
-    $filePath = "";
-    $certificate_images = [];
-
-    $certificate_query = "SELECT filePath FROM certificate";
-    $certificate_result = mysqli_query($conn, $certificate_query);
-
-    if ($certificate_result && mysqli_num_rows($certificate_result) > 0) {
-        while ($row = mysqli_fetch_assoc($certificate_result)) {
-            $certificate_images[] = $row['filePath'];
-        }
-    }
 ?>
 
     <div class="breadcrumb__area" style="background-image: url('assets/img/certifications.jpg');">
@@ -35,13 +23,33 @@ include "./components/navbar.php";
     <div class="two__columns section-padding-three">
         <div class="container">
             <div class="row">
-                <?php foreach ($certificate_images as $certificate_Path): ?>
+                <?php
+
+                    $select_query = "SELECT * FROM certificate";
+
+                    $result = mysqli_query($conn, $select_query);
+
+                    // Check for query errors
+                    if (!$result) {
+                        die("Query failed: " . mysqli_error($conn));
+                    }
+
+                    if (mysqli_num_rows($result) > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            $certificateID = $row['certificateID'];
+                            $title = $row['title'];
+                            $filePath = $row['filePath'];
+                        
+                ?>
                     <div class="col-md-4 mt-25 mb-50 wow fadeInUp" data-wow-delay=".4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
                         <div class="portfolio__two-item">
-                            <img src="https://backoffice.giolee.com/<?php echo $certificate_Path; ?>" alt="image">
+                            <img src="https://backoffice.giolee.com/<?php echo $filePath; ?>" alt="image">
                         </div>
                     </div>
-                <?php endforeach; ?>
+                <?php
+                        }
+                    }
+                ?>
             </div>
         </div>
     </div>
