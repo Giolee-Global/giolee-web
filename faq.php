3558ca0f16260aee8eb1793e929f1e49278e9b21
codystack <ebuzzadvert@gmail.com>
2025-07-17 15:49:15 +0100

    FAQ Added

--- faq.php
+++ faq.php
@@ -1,4 +1,5 @@
 <?php
+require_once "./config/db.php";
 include "./components/header.php";
 include "./components/navbar.php";
 ?>
@@ -24,60 +25,35 @@ include "./components/navbar.php";
             <div class="row jc-center">
                 <div class="col-xl-10">
                     <div class="wow fadeInUp" data-wow-delay=".4s" id="accordionExample">
+                        <?php
+
+                            $select_query = "SELECT * FROM faq";
+
+                            $result = mysqli_query($conn, $select_query);
+
+                            // Check for query errors
+                            if (!$result) {
+                                die("Query failed: " . mysqli_error($conn));
+                            }
+
+                            if (mysqli_num_rows($result) > 0) {
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    $faqID = $row['faqID'];
+                                    $question = $row['question'];
+                                    $designation = $row['designation'];
+                                    $answer = $row['answer'];
+                                
+                        ?>
                         <div class="faq-item">
-                            <h5 class="icon" data-bs-toggle="collapse" data-bs-target="#collapseOne">How do I know if my business is compliant with environmental regulations?</h5>
-                            <div id="collapseOne" class="faq-item-body collapse show" data-bs-parent="#accordionExample">
-                                <p>Our team can conduct an environmental compliance audit to assess your business’s adherence to local, state, and federal environmental regulations. We can also assist with obtaining necessary permits and help you implement corrective actions if needed.</p>
+                            <h5 class="icon collapsed" data-bs-toggle="collapse" data-bs-target="#collapse<?php echo $faqID; ?>"><?php echo $question; ?></h5>
+                            <div id="collapse<?php echo $faqID; ?>" class="faq-item-body collapse" data-bs-parent="#accordionExample">
+                                <p><?php echo $answer; ?></p>
                             </div>
                         </div>
-                        <div class="faq-item">
-                            <h5 class="icon collapsed" data-bs-toggle="collapse" data-bs-target="#collapseTwo">Do you provide environmental training or educational services?</h5>
-                            <div id="collapseTwo" class="faq-item-body collapse" data-bs-parent="#accordionExample">
-                                <p>Yes, we provide training sessions and workshops for employees and organizations on topics like environmental compliance, sustainable practices, environmental impact assessment, and energy conservation.</p>
-                            </div>
-                        </div>
-                        <div class="faq-item">
-                            <h5 class="icon collapsed" data-bs-toggle="collapse" data-bs-target="#collapseThree">How can I get started with your services?</h5>
-                            <div id="collapseThree" class="faq-item-body collapse" data-bs-parent="#accordionExample">
-                                <p>Getting started is simple! You can contact us through our website or call us directly to schedule a consultation. We’ll assess your needs and propose a tailored plan to help you meet your environmental goals.</p>
-                            </div>
-                        </div>
-                        <div class="faq-item">
-                            <h5 class="icon collapsed" data-bs-toggle="collapse" data-bs-target="#collapseFour">Are your services available nationwide?</h5>
-                            <div id="collapseFour" class="faq-item-body collapse" data-bs-parent="#accordionExample">
-                                <p>Yes, we provide services across the country. We have a network of partners and local experts to ensure that we can meet the unique needs of your location.</p>
-                            </div>
-                        </div>
-                        <div class="faq-item">
-                            <h5 class="icon collapsed" data-bs-toggle="collapse" data-bs-target="#collapseFive">Why should I choose your company over other environmental firms?</h5>
-                            <div id="collapseFive" class="faq-item-body collapse" data-bs-parent="#accordionExample">
-                                <p>At Giolee Global Resources our team has extensive experience, a commitment to sustainability, and a customer-focused approach. We offer innovative solutions that align with both regulatory requirements and best environmental practices, ensuring long-term benefits for your business and the environment.</p>
-                            </div>
-                        </div>
-                        <div class="faq-item">
-                            <h5 class="icon collapsed" data-bs-toggle="collapse" data-bs-target="#collapseSix">What Remediation services do you offer?</h5>
-                            <div id="collapseSix" class="faq-item-body collapse" data-bs-parent="#accordionExample">
-                                <p>We provide soil and water remediation and site decontamination.</p>
-                            </div>
-                        </div>
-                        <div class="faq-item">
-                            <h5 class="icon collapsed" data-bs-toggle="collapse" data-bs-target="#collapseSeven">How do you dispose of Hazardous waste?</h5>
-                            <div id="collapseSeven" class="faq-item-body collapse" data-bs-parent="#accordionExample">
-                                <p>We follow strict disposal procedures, transporting hazardous waste to certified disposal or treatment facilities following environmental guidelines and laws.</p>
-                            </div>
-                        </div>
-                        <div class="faq-item">
-                            <h5 class="icon collapsed" data-bs-toggle="collapse" data-bs-target="#collapseEight">How do you minimize environmental impact during cleanup?</h5>
-                            <div id="collapseEight" class="faq-item-body collapse" data-bs-parent="#accordionExample">
-                                <p>We use eco-friendly cleaning agents, advanced technological techniques and sustainable disposal methods to reduce environmental harm.</p>
-                            </div>
-                        </div>
-                        <div class="faq-item">
-                            <h5 class="icon collapsed" data-bs-toggle="collapse" data-bs-target="#collapseNine">How can we engage your services?</h5>
-                            <div id="collapseNine" class="faq-item-body collapse" data-bs-parent="#accordionExample">
-                                <p>You can either visit us at our Head office or call <b>+234-7031513161</b> or send us an email at <b><a href="mailto:info@giolee.com">info@giolee.com</a></b> and we will provide a feedback within 24hours.</p>
-                            </div>
-                        </div>
+                        <?php
+                                }
+                            }
+                        ?>
                     </div>
                 </div>
             </div>
